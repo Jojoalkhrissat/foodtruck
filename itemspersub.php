c00f8b36f8652ac49bf5015291a5cdad61ab6e8d
yousef alkrisat <jo_alkrissat@yahoo.com>
2020-09-27 19:32:49 +0300

    changed parameter receiving

--- itemspersub.php
+++ itemspersub.php
@@ -3,7 +3,6 @@ require "connect.php";
 if ($_SERVER["REQUEST_METHOD"] == "GET"){
 		$subcategory=$_GET['subcategory'];
 	try{
-
 $MyJsonData1="";
 $itemspersub="SELECT `id`, `Name`, `Shop`, `Photo`, `Price`, `TimesSold` FROM `items` where `SubCategory`='$subcategory' limit 20 ";
 $getitemspersub = $conn->query($itemspersub);
@@ -17,7 +16,7 @@ echo $MyJsonData1;
 echo ']';
 }catch(Exception $e){
 echo "400 bad request";	
-}
+}}else{
 echo "400 bad request";
 }
 ?>
