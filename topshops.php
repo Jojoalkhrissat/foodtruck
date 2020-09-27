1175d4d64446cfb5cd5e0e4ae9660619115ad310
yousef alkrisat <jo_alkrissat@yahoo.com>
2020-09-27 22:30:05 +0300

    changed parameter receiving

--- topshops.php
+++ topshops.php
@@ -1,7 +1,7 @@
 <?php
 require "connect.php";
 if(($_SERVER["REQUEST_METHOD"] == "GET")){
-$topshops='SELECT id,ShopName,Photo FROM shop WHERE ExtraPay>0';
+$topshops='SELECT DISTINCT S.'.'id,S.'.'ShopName,S.'.'Photo,Avg(F.Rating) as Rating,count(F.Rating) as RatingCount FROM shop as S inner join feedback as F on F.shop=S.id WHERE ExtraPay > 0 GROUP by F.shop order by Rating DESC limit 10';
 $getshops = $conn->query($topshops);
 $getshops->setFetchMode(PDO::FETCH_ASSOC);
 echo '[';
